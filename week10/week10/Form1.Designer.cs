b8e7fd1a81dedc83c92ff131ef8504baa20ee6fb
Máté Bukovácz <mate.bukovacz@stud.uni-corvinus.hu>
2022-12-09 12:41:33 +0100

    week10 finish

--- week10/week10/Form1.Designer.cs
+++ week10/week10/Form1.Designer.cs
@@ -29,13 +29,47 @@ namespace week10
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.genLbl = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // genLbl
+            // 
+            this.genLbl.AutoSize = true;
+            this.genLbl.Location = new System.Drawing.Point(36, 13);
+            this.genLbl.Name = "genLbl";
+            this.genLbl.Size = new System.Drawing.Size(35, 13);
+            this.genLbl.TabIndex = 0;
+            this.genLbl.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.genLbl);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label genLbl;
+        private System.Windows.Forms.Button button1;
     }
 }
 
